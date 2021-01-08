b0d7f8b165c4754ddb913a3ee097bd5a77060e5f
Deleon Francis <dxfrancis.coding@gmail.com>
2021-01-08 08:35:01 -0500

    added icons to the page

--- Develop/Script.js
+++ Develop/Script.js
@@ -58,6 +58,16 @@ $(document).ready(function () {
           },
         });
 
+        // Get Icon and put it on the page
+        var todayIconLocation = response.weather[0].icon;
+        var todayIconURL =
+          "https://openweathermap.org/img/wn/" + todayIconLocation + "@2x.png";
+
+        // UPDATE ICON FOR MAIN WEATHER CARD
+        var todayIconImage = $("<img>");
+        todayIconImage.attr("src", todayIconURL);
+        $("#today-icon").html(todayIconImage);
+
         // api for 5 Day weather
         $.ajax({
           method: "GET",
@@ -74,31 +84,71 @@ $(document).ready(function () {
             $("#day1").text(day1);
             $("#temp1").text(response3.list[1].main.temp.toFixed(0));
             $("#humid1").text(response3.list[1].main.humidity);
-            $("")
+            $("");
+            // Icon for Day 1
+            var iconDay1Location = response3.list[1].weather[0].icon;
+            var iconDay1Url =
+              "https://openweathermap.org/img/wn/" + iconDay1Location + ".png";
 
-            // Day Tow
+            var iconImageDay1 = $("<img>");
+            iconImageDay1.attr("src", iconDay1Url);
+            $("#icon-day1").html(iconImageDay1);
+
+            // Day Two
             let day2 = moment().add(2, "days").format("L");
             $("#day2").html(day2);
             $("#temp2").text(response3.list[8].main.temp.toFixed(0));
             $("#humid2").text(response3.list[8].main.humidity);
+            // Icon for Day 2
+            var iconDay2Location = response3.list[8].weather[0].icon;
+            var iconDay2Url =
+              "https://openweathermap.org/img/wn/" + iconDay2Location + ".png";
+
+            var iconImageDay2 = $("<img>");
+            iconImageDay2.attr("src", iconDay2Url);
+            $("#icon-day2").html(iconImageDay2);
 
             // Day Three
             let day3 = moment().add(3, "days").format("L");
             $("#day3").html(day3);
             $("#temp3").text(response3.list[16].main.temp.toFixed(0));
             $("#humid3").text(response3.list[16].main.humidity);
+            // Icon for Day 3
+            var iconDay3Location = response3.list[16].weather[0].icon;
+            var iconDay3Url =
+              "https://openweathermap.org/img/wn/" + iconDay3Location + ".png";
+
+            var iconImageDay3 = $("<img>");
+            iconImageDay3.attr("src", iconDay3Url);
+            $("#icon-day3").html(iconImageDay3);
 
             // Day Four
             let day4 = moment().add(4, "days").format("L");
             $("#day4").html(day4);
             $("#temp4").text(response3.list[24].main.temp.toFixed(0));
             $("#humid4").text(response3.list[24].main.humidity);
+            // Icon for Day 4
+            var iconDay4Location = response3.list[24].weather[0].icon;
+            var iconDay4Url =
+              "https://openweathermap.org/img/wn/" + iconDay4Location + ".png";
+
+            var iconImageDay4 = $("<img>");
+            iconImageDay4.attr("src", iconDay4Url);
+            $("#icon-day4").html(iconImageDay4);
 
             // Day Five
             let day5 = moment().add(5, "days").format("L");
             $("#day5").html(day5);
             $("#temp5").text(response3.list[32].main.temp.toFixed(0));
             $("#humid5").text(response3.list[32].main.humidity);
+            // Icon for Day 5
+            var iconDay5Location = response3.list[32].weather[0].icon;
+            var iconDay5Url =
+              "https://openweathermap.org/img/wn/" + iconDay5Location + ".png";
+
+            var iconImageDay5 = $("<img>");
+            iconImageDay5.attr("src", iconDay5Url);
+            $("#icon-day5").html(iconImageDay5);
           },
         });
       },
