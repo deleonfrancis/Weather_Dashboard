77b9ed4bc8e1c126a8b11af850d4e1884234b4b7
Deleon Francis <dxfrancis.coding@gmail.com>
2021-07-22 14:10:31 -0400

    added footer with copyright year.

---
+++ assets/footerCopyright.js
@@ -0,0 +1,7 @@
+const year =  new Date().getFullYear()
+
+document.getElementById("my-Copyright").innerHTML +=
+    `© ${year} Copyright:  <a class="text-white" target="_blank" href="https://dxfrancis.com/"
+    >Deleon Xavier Francis</a
+  >`
+
